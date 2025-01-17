fc6f0fce3dea2415d7199c8d8ae0dfe6af19b791
omarbarbeir <omarbarbeir2@gmail.com>
2025-01-17 21:53:54 +0200

    some updates

--- src/Pages/Home.js
+++ src/Pages/Home.js
@@ -10,11 +10,11 @@ let Home =()=>{
                     
                     {data.map((e,index)=>{
                         return(
-                            <div class=" bg-white border border-gray-200  rounded-lg shadow dark:bg-gray-800 dark:border-gray-700 flex justify-center items-center flex-col h-[360px] w-[360px] md:w-[470px] md:h-[460px] lg:w-[550px] lg:h-[560px] mt-5 transition-all">
+                            <div class=" bg-white border border-gray-200  rounded-lg shadow  flex justify-center items-center flex-col h-[360px] w-[360px] md:w-[470px] md:h-[460px] lg:w-[550px] lg:h-[560px] mt-5 transition-all">
                                     <img class="rounded-t-lg w-full h-full" src={e.img} alt="" />
                                 <div class="p-2  flex justify-center items-center flex-col text-xl">
-                                        <h5 class="mb-2 text-3xl md:text-5xl lg:mb-5 font-bold tracking-tight text-gray-900 dark:text-white">{e.title1}</h5>
-                                        <h5 class="mb-2 text-3xl md:text-5xl lg:mb-5 font-bold tracking-tight text-gray-900 dark:text-white">{e.title2}</h5>
+                                        <h5 class="mb-2 text-3xl md:text-5xl lg:mb-5 font-bold tracking-tight text-gray-900">{e.title1}</h5>
+                                        <h5 class="mb-2 text-3xl md:text-5xl lg:mb-5 font-bold tracking-tight text-gray-900">{e.title2}</h5>
                                     <Link  className="w-[300px] h-[50px] rounded-md bg-[#e97156] hover:bg-[#D6563A] transition-all text-2xl font-bold flex justify-center items-center" to={e.path}><button>ابدأ</button></Link>
                                     
                                 </div>
--- src/Pages/MosalsalatAfter.js
+++ src/Pages/MosalsalatAfter.js
@@ -1,6 +1,102 @@
 export let Mos = [
     {
-        image:require("../imgs/mos/Ezoo.jpeg"),
-        item:"يتربي في عزه"
-    }
+        image:require("../imgs/mos/2000/awan elward.png"),
+        item:"اوان الورد"
+    },
+    {
+        image:require("../imgs/mos/2000/hadeqat elshar.jpg"),
+        item:"حديقة الشر"
+    },
+    {
+        image:require("../imgs/mos/2000/keta fe shoq elsamak.jpg"),
+        item:"قطة في سوق السمك"
+    },
+    {
+        image:require("../imgs/mos/2000/khayal elzel.jpg"),
+        item:"خيال الظل"
+    },
+    {
+        image:require("../imgs/mos/2000/kot we far five star.webp"),
+        item:"قط و فار فايف ستار"
+    },
+    {
+        image:require("../imgs/mos/2000/naseem elrooh.jpg"),
+        item:"نسيم الروح"
+    },
+    {
+        image:require("../imgs/mos/2000/obra ayda.jpg"),
+        item:"أوبرا عايدة"
+    },
+    {
+        image:require("../imgs/mos/2000/robabeqya.jpg"),
+        item:"روبابيكيا"
+    },
+    {
+        image:require("../imgs/mos/2000/samhony makansh kasdy.jpg"),
+        item:"سامحوني ماكنش قصدي"
+    },
+    {
+        image:require("../imgs/mos/2000/the family and the people.jpg"),
+        item:"العائله و الناس"
+    },
+    {
+        image:require("../imgs/mos/2000/ya regal elalam etahedo.jpg"),
+        item:"يا رجال العالم اتحدوا"
+    },
+    {
+        image:require("../imgs/mos/2000/zezenya.jpg"),
+        item:"زيزينيا"
+    },
+    {
+        image:require("../imgs/mos/2001/ahl eldonya.jpg"),
+        item:"أهل الدنيا"
+    },
+    {
+        image:require("../imgs/mos/2001/alnesaa kademoon.jpg"),
+        item:"النساء قادمون"
+    },
+    {
+        image:require("../imgs/mos/2001/bawabet elhalawany.jpg"),
+        item:"بوابة الحلواني"
+    },
+    {
+        image:require("../imgs/mos/2001/elbar elgharby.jpg"),
+        item:"البر الغربي"
+    },
+    {
+        image:require("../imgs/mos/2001/elhag metwally.jpg"),
+        item:"عائلة الحاج متولي"
+    },
+    {
+        image:require("../imgs/mos/2001/elsera elhelalya.jpeg"),
+        item:"السيرة الهلالية"
+    },
+    {
+        image:require("../imgs/mos/2001/hadeeth elsabah w el masaa.jpg"),
+        item:"حديث الصباح و المساء"
+    },
+    {
+        image:require("../imgs/mos/2001/hamza w banatoo elkhamsa.jpg"),
+        item:"حمزة و بناته الخمسة"
+    },
+    {
+        image:require("../imgs/mos/2001/hawary we kosoor.jpg"),
+        item:"حواري و قصور"
+    },
+    {
+        image:require("../imgs/mos/2001/lel adalla wegooh katheraa.jpg"),
+        item:"للعدالة وجوه كثيرة"
+    },
+    {
+        image:require("../imgs/mos/2001/ragol fe zaman elawlama.jpg"),
+        item:"رجل في زمن العولمة"
+    },
+    {
+        image:require("../imgs/mos/2001/sooq elasr.jpeg"),
+        item:"سوق العصر"
+    },
+    {
+        image:require("../imgs/mos/2001/zabt we ehdaar.jpg"),
+        item:"ضبط و إحضار"
+    },
 ]
